6c064739d00a55fc3897d07865ca3120e9af5aae
2cverde18 <00001079556497sp@al.educacao.sp.gov.br>
2024-11-14 12:07:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Português',
+    'O que é Ditongo?',
+    'É o encontro de duas vogais'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é fotossíntese?',
+    'É como as plantas fazem alimento usando luz.'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o principal motivo da Revolução Francesa?',
+    'O principal motivo foi a desigualdade social e ecônomica na França'
+)
+
+criaCartao(
+    'geografia',
+    'Qual o nome do último planeta do Sistema Solar?',
+    'O nome do último planeta do Sistema Solar se chama netuno'
+)
